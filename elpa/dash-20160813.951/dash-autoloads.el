53054838733f0209b90aa1876e90702adc1569d9
Bruce Chen <flyingns@gmail.com>
2016-08-15 21:00:59 +0800

    update elpa

---
+++ elpa/dash-20160813.951/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22449 46451 882643 485000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
--- elpa/spacemacs-theme-20160806.515/spacemacs-theme-autoloads.el
+++
@@ -1,24 +0,0 @@
-;;; spacemacs-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "spacemacs-common" "spacemacs-common.el" (22437
-;;;;;;  61526 694174 620000))
-;;; Generated autoloads from spacemacs-common.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("spacemacs-dark-theme.el" "spacemacs-light-theme.el"
-;;;;;;  "spacemacs-theme-pkg.el") (22437 61526 801149 395000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; spacemacs-theme-autoloads.el ends here
